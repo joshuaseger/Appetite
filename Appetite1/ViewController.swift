ad26662d7ee22dc971f3db78da3f3b51eb8884ac
joshuaseger <joshua.seger@hudl.com>
2015-02-26 19:08:15 -0600

    Initial Commit

---
+++ Appetite1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Appetite1
+//
+//  Created by Joshua Seger on 2/26/15.
+//  Copyright (c) 2015 JoshuaSeger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
