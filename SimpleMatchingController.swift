a253b773cf18666b9c6a2c5207085f5ada2615d6
joshuaseger <joshua.seger@hudl.com>
2015-04-07 20:17:06 -0500

    Created new Branch UserSideOfApp

--- SimpleMatchingController.swift
+++ SimpleMatchingController.swift
@@ -2,7 +2,7 @@
 //  SimpleMatchingController.swift
 //  Appetite1
 //
-//  Created by Joshua Seger on 4/3/15.
+//  Created by Joshua Seger on 4/7/15.
 //  Copyright (c) 2015 JoshuaSeger. All rights reserved.
 //
 import Foundation
