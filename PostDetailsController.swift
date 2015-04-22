198412b91f0ff5cf436dbc8b87b1b2445bb90f4b
Joshua Seger <joshuaseger@LOANER65M.ddns.creighton.edu>
2015-04-21 20:46:00 -0500

    Added Scrollview

--- PostDetailsController.swift
+++ PostDetailsController.swift
@@ -8,42 +8,64 @@
 
 import UIKit
 
-class PostDetailsController: UIViewController {
+class PostDetailsController: UIViewController, UIScrollViewDelegate {
 
+
+    @IBOutlet var containerView: UIView!
     @IBOutlet var scrollView: UIScrollView!
     var restaurant: PFObject?
-    @IBOutlet var restaurantNameLabel: UILabel!
 
-    @IBOutlet var RestaurantImage: UIImageView!
-    @IBOutlet var priceLabel: UILabel!
-    @IBOutlet var phoneLabel: UILabel!
-    @IBOutlet var addressLabel: UILabel!
-    @IBOutlet var cityLabel: UILabel!
-    @IBOutlet var stateLabel: UILabel!
-    @IBOutlet var zipLabel: UILabel!
 
-    override func loadView() {
-
-        
-    }
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var x: PFObject = restaurant?.fetchIfNeeded() as PFObject!
-        //Controller was only passed object reference.  Must fetch actual object from Parse now.
-
-        
-
-
-        
-        
-        
-        
-
+   //     var x: PFObject = restaurant?.fetchIfNeeded() as PFObject!
+        //Controller was only passed object reference.  Must fetch actual object from Parse now
         
         // Do any additional setup after loading the view.
+    
+        scrollView.contentSize = containerView.frame.size;
+        
+        // Set up the minimum & maximum zoom scale
+        let scrollViewFrame = scrollView.frame
+        let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
+        let scaleHeight = scrollViewFrame.size.height / scrollView.contentSize.height
+        let minScale = min(scaleWidth, scaleHeight)
+        
+        scrollView.minimumZoomScale = minScale
+        scrollView.maximumZoomScale = 1.0
+        scrollView.zoomScale = 1.0
+        
+        centerScrollViewContents()
     }
     
+    func centerScrollViewContents() {
+        let boundsSize = scrollView.bounds.size
+        var contentsFrame = containerView.frame
+        
+        if contentsFrame.size.width < boundsSize.width {
+            contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0
+        } else {
+            contentsFrame.origin.x = 0.0
+        }
+        
+        if contentsFrame.size.height < boundsSize.height {
+            contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0
+        } else {
+            contentsFrame.origin.y = 0.0
+        }
+        
+        containerView.frame = contentsFrame
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return containerView
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        centerScrollViewContents()
+    }
 
 
     override func didReceiveMemoryWarning() {
